b9298bfaad2c3a1ccd12c674015a9d85dda88724
kalpeshmohanta <kalpeshmohanta@gmail.com>
2025-01-04 00:24:53 +0530

    6th

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -4,5 +4,18 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui-vendor': ['@mui/material', '@mui/icons-material'],
+          'video-vendor': ['video.js', 'videojs-youtube'],
+          'utils-vendor': ['framer-motion', 'react-slick', 'slick-carousel']
+        }
+      }
+    }
+  }
 })
